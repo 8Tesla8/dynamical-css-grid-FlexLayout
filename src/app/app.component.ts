7b3ca033e1255a7c0e23e78662c671e2360e155b
YuriiKuznietsov <kela256@gmail.com>
2021-06-03 08:37:49 +0300

    add state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,107 +1,167 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
+
+  public gridAreas = "";
+
+  public state = new State();
+  public previewState = new State();
+
+  public showGrid(): void {
+    this.previewState.topLeftVisibility = this.state.topLeftVisibility;
+    this.previewState.topRightVisibility = this.state.topRightVisibility;
+    this.previewState.topMiddleVisibility = this.state.topMiddleVisibility;
+    this.previewState.middleRightVisibility = this.state.middleRightVisibility;
+    this.previewState.bottomLeftVisibility = this.state.bottomLeftVisibility;
+    this.previewState.bottomRightVisibility = this.state.bottomRightVisibility;
+
+    this.gridAreas = this.getGridAreas();
+  }
+
+  public getGridAreas(): string {
+    let topLeftArea = "topLeft ";
+    let topMiddleArea = "topMiddle ";
+    let topRightArea = "topRight ";
+    let middleRightArea = "middleRight ";
+    let bottomLeftArea = "bottomLeft ";
+    let bottomRightArea = "bottomRight ";
+
+    let rowOne = "";
+    let rowTwo = "";
+    let rowThree = "";
+
+    //row one
+    //show one part
+    if (
+      this.state.topLeftVisibility &&
+      !this.state.topMiddleVisibility &&
+      !this.state.topRightVisibility
+    ) {
+      rowOne += topLeftArea + topLeftArea + topLeftArea + topLeftArea;
+    } else if (
+      !this.state.topLeftVisibility &&
+      this.state.topMiddleVisibility &&
+      !this.state.topRightVisibility
+    ) {
+      rowOne += topMiddleArea + topMiddleArea + topMiddleArea + topMiddleArea;
+    } else if (
+      !this.state.topLeftVisibility &&
+      !this.state.topMiddleVisibility &&
+      this.state.topRightVisibility
+    ) {
+      rowOne += topRightArea + topRightArea + topRightArea + topRightArea;
+    }
+    //show two parts
+    else if (
+      this.state.topLeftVisibility &&
+      this.state.topMiddleVisibility &&
+      !this.state.topRightVisibility
+    ) {
+      rowOne += topLeftArea + topMiddleArea + topMiddleArea + topMiddleArea;
+    } else if (
+      this.state.topLeftVisibility &&
+      !this.state.topMiddleVisibility &&
+      this.state.topRightVisibility
+    ) {
+      rowOne += topLeftArea + topLeftArea + topRightArea + topRightArea;
+    } else if (
+      !this.state.topLeftVisibility &&
+      this.state.topMiddleVisibility &&
+      this.state.topRightVisibility
+    ) {
+      rowOne += topMiddleArea + topMiddleArea + topMiddleArea + topRightArea;
+    }
+    //show three parts
+    else if (
+      this.state.topLeftVisibility &&
+      this.state.topMiddleVisibility &&
+      this.state.topRightVisibility
+    ) {
+      rowOne += topLeftArea + topMiddleArea + topMiddleArea + topRightArea;
+    }
+
+    //row two
+    if (this.state.middleRightVisibility && !this.state.bottomLeftVisibility) {
+      //show only middleRight
+      rowTwo =
+        middleRightArea + middleRightArea + middleRightArea + middleRightArea;
+    } else if (
+      this.state.middleRightVisibility &&
+      this.state.bottomLeftVisibility
+    ) {
+      rowTwo =
+        bottomLeftArea + bottomLeftArea + middleRightArea + middleRightArea;
+    }
+    //do not show middleRight
+    else if (
+      !this.state.middleRightVisibility &&
+      this.state.bottomLeftVisibility &&
+      this.state.bottomRightVisibility
+    ) {
+      rowTwo =
+        bottomLeftArea + bottomLeftArea + bottomRightArea + bottomRightArea;
+    } else if (
+      !this.state.middleRightVisibility &&
+      this.state.bottomLeftVisibility &&
+      !this.state.bottomRightVisibility
+    ) {
+      rowTwo =
+        bottomLeftArea + bottomLeftArea + bottomLeftArea + bottomLeftArea;
+    } else if (
+      !this.state.middleRightVisibility &&
+      !this.state.bottomLeftVisibility &&
+      this.state.bottomRightVisibility
+    ) {
+      rowTwo =
+        bottomRightArea + bottomRightArea + bottomRightArea + bottomRightArea;
+    }
+
+    //row three
+    if (this.state.bottomLeftVisibility && this.state.bottomRightVisibility) {
+      rowThree =
+        bottomLeftArea + bottomLeftArea + bottomRightArea + bottomRightArea;
+    } else if (
+      this.state.bottomLeftVisibility &&
+      !this.state.bottomRightVisibility
+    ) {
+      rowThree =
+        bottomLeftArea + bottomLeftArea + bottomLeftArea + bottomLeftArea;
+    } else if (
+      !this.state.bottomLeftVisibility &&
+      this.state.bottomRightVisibility
+    ) {
+      rowThree =
+        bottomRightArea + bottomRightArea + bottomRightArea + bottomRightArea;
+    }
+
+    if (rowOne === "") {
+      rowOne = rowTwo;
+      rowTwo = rowThree;
+    }
+    if (rowTwo === "") {
+      rowTwo = rowOne;
+      rowThree = rowOne;
+    }
+    if (rowThree === "") {
+      rowThree = rowTwo;
+    }
+
+    let gridArea = rowOne + " | " + rowTwo + " | " + rowThree;
+
+    return gridArea;
+  }
+}
+
+export class State {
   public topLeftVisibility = true;
   public topMiddleVisibility = true;
   public topRightVisibility = true;
   public middleRightVisibility = true;
   public bottomLeftVisibility = true;
   public bottomRightVisibility = true;
-
-  public gridAreas = "";
-
-  public showGrid():void{
-    this.gridAreas = this.getGridAreas();
-  }
-
-  public getGridAreas(): string {
-    let topLeftArea = 'topLeft ';
-    let topMiddleArea = 'topMiddle ';
-    let topRightArea = 'topRight ';
-    let middleRightArea = 'middleRight ';
-    let bottomLeftArea = 'bottomLeft ';
-    let bottomRightArea = 'bottomRight ';
-
-
-    let rowOne = '';
-    let rowTwo = '';
-    let rowThree = '';
-
-    //row one
-    //show one part
-    if (this.topLeftVisibility && !this.topMiddleVisibility && !this.topRightVisibility) {
-        rowOne += topLeftArea + topLeftArea + topLeftArea + topLeftArea;
-    }
-    else if(!this.topLeftVisibility && this.topMiddleVisibility && !this.topRightVisibility){
-        rowOne += topMiddleArea + topMiddleArea + topMiddleArea + topMiddleArea;
-    }
-    else if (!this.topLeftVisibility && !this.topMiddleVisibility && this.topRightVisibility) {
-        rowOne += topRightArea + topRightArea + topRightArea + topRightArea;
-    }
-    //show two parts
-    else if(this.topLeftVisibility && this.topMiddleVisibility && !this.topRightVisibility){
-        rowOne += topLeftArea + topMiddleArea + topMiddleArea + topMiddleArea;
-    } 
-    else if(this.topLeftVisibility && !this.topMiddleVisibility && this.topRightVisibility){
-        rowOne += topLeftArea + topLeftArea + topRightArea + topRightArea;
-    } 
-    else if(!this.topLeftVisibility && this.topMiddleVisibility && this.topRightVisibility){
-        rowOne += topMiddleArea + topMiddleArea + topMiddleArea + topRightArea;
-    }
-    //show three parts 
-    else if(this.topLeftVisibility && this.topMiddleVisibility && this.topRightVisibility){
-        rowOne += topLeftArea + topMiddleArea + topMiddleArea + topRightArea;
-    }
-
-
-    //row two
-    if(this.middleRightVisibility && !this.bottomLeftVisibility){ //show only middleRight
-        rowTwo = middleRightArea + middleRightArea + middleRightArea + middleRightArea;
-    }
-    else if(this.middleRightVisibility && this.bottomLeftVisibility){
-        rowTwo = bottomLeftArea + bottomLeftArea + middleRightArea + middleRightArea;
-    }
-    //do not show middleRight
-    else if(!this.middleRightVisibility && this.bottomLeftVisibility && this.bottomRightVisibility){
-        rowTwo = bottomLeftArea + bottomLeftArea + bottomRightArea + bottomRightArea;
-    }
-    else if(!this.middleRightVisibility && this.bottomLeftVisibility && !this.bottomRightVisibility){
-        rowTwo = bottomLeftArea + bottomLeftArea + bottomLeftArea + bottomLeftArea;
-    }
-    else if(!this.middleRightVisibility && !this.bottomLeftVisibility && this.bottomRightVisibility){
-        rowTwo = bottomRightArea + bottomRightArea + bottomRightArea + bottomRightArea;
-    }
-
-
-    //row three
-    if(this.bottomLeftVisibility && this.bottomRightVisibility){
-        rowThree = bottomLeftArea + bottomLeftArea + bottomRightArea + bottomRightArea;
-    }
-    else if(this.bottomLeftVisibility && !this.bottomRightVisibility){
-        rowThree = bottomLeftArea + bottomLeftArea + bottomLeftArea + bottomLeftArea;
-    }
-    else if(!this.bottomLeftVisibility && this.bottomRightVisibility){
-        rowThree = bottomRightArea + bottomRightArea + bottomRightArea + bottomRightArea;
-    }
-
-
-    //row one is empty copy row two
-    if(rowOne === ""){
-        rowOne = rowTwo;
-        rowTwo = rowThree;
-    } 
-    else if(rowTwo === ""){
-        rowTwo = rowOne;
-        rowThree = rowOne;
-    }
-
-    let gridArea = rowOne + ' | ' + rowTwo + ' | ' + rowThree;
-
-    return gridArea;
-}
 }
